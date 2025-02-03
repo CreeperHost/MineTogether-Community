78d073d9e67f16b98d961717bbf0b03031a9346b
brandon3055 <brandon4261@gmail.com>
2025-02-03 15:13:24 +1100

    Lang Data Gen (Not working)

--- neoforge/src/main/java/net/creeperhost/minetogethercommunity/neoforge/datagen/LangGenerator.java
+++ neoforge/src/main/java/net/creeperhost/minetogethercommunity/neoforge/datagen/LangGenerator.java
@@ -1,7 +1,25 @@
 package net.creeperhost.minetogethercommunity.neoforge.datagen;
 
+
+import net.creeperhost.minetogethercommunity.MineTogether;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.LanguageProvider;
+
 /**
  * Created by brandon3055 on 02/02/2025
  */
-public class LangGenerator {
+public class LangGenerator extends LanguageProvider {
+
+    public LangGenerator(PackOutput output) {
+        super(output, MineTogether.MOD_ID, "en_us");
+    }
+
+    @Override
+    protected void addTranslations() {
+
+        
+
+
+
+    }
 }
