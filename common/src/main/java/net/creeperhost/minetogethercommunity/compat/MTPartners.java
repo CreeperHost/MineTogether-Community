35b521c9c78fe5875ab3708a5992022627f23ceb
Gigabit101 <thegigabit101@gmail.com>
2026-06-17 11:04:12 +0100

    Auto stash before merge of "1.21cosmetics" and "origin/1.21cosmetics"

--- common/src/main/java/net/creeperhost/minetogethercommunity/compat/MTPartners.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/compat/MTPartners.java
@@ -1,23 +1,12 @@
 package net.creeperhost.minetogethercommunity.compat;
 
 import net.creeperhost.polylib.client.modulargui.ModularGui;
-import net.minecraft.client.gui.screens.Screen;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import net.creeperhost.minetogetherpartners.orderform.OrderGui;
 
 public class MTPartners
 {
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    public static boolean openOrderUI(ModularGui gui) {
-        try {
-            Class<?> screenClass = Class.forName("net.creeperhost.minetogetherpartners.orderform.OrderGui$Screen");
-            Screen screen = (Screen) screenClass.getConstructor(Screen.class, boolean.class).newInstance(gui.getScreen(), true);
-            gui.mc().setScreen(screen);
-            return true;
-        } catch (ReflectiveOperationException | LinkageError e) {
-            LOGGER.error("Failed to open minetogetherpartners order form.", e);
-            return false;
-        }
+    public static void openOrderUI(ModularGui gui) {
+        //TODO fix this before release
+//        gui.mc().setScreen(new OrderGui.Screen(gui.getScreen(), true));
     }
 }
