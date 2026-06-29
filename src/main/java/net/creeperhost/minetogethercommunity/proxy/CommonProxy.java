182bc159dffa8f79f5588c929c1601ff9eb10a5f
Paul Taylor <paul_t@creeperhost.net>
2026-06-29 15:11:18 +0100

    Add dedicated server MineTogether Connect

--- src/main/java/net/creeperhost/minetogethercommunity/proxy/CommonProxy.java
+++ src/main/java/net/creeperhost/minetogethercommunity/proxy/CommonProxy.java
@@ -1,7 +1,12 @@
 package net.creeperhost.minetogethercommunity.proxy;
 
+import net.creeperhost.minetogethercommunity.connect.DedicatedServerConnect;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class CommonProxy {
 
@@ -9,5 +14,13 @@ public class CommonProxy {
     }
 
     public void init(FMLInitializationEvent event) {
+        FMLCommonHandler.instance().bus().register(this);
+    }
+
+    @SubscribeEvent
+    public void playerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.player instanceof EntityPlayerMP) {
+            DedicatedServerConnect.playerJoined((EntityPlayerMP) event.player);
+        }
     }
 }
