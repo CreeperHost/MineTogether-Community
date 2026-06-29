51ef626f0da94d92da842af306486417a697919b
Paul Taylor <paul_t@creeperhost.net>
2026-06-29 20:02:59 +0100

    fix: harden ftb pack companion pause compat

--- common/src/main/java/net/creeperhost/minetogethercommunity/compat/Integration.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/compat/Integration.java
@@ -1,6 +1,8 @@
 package net.creeperhost.minetogethercommunity.compat;
 
 import dev.architectury.platform.Platform;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.function.Supplier;
 
@@ -9,9 +11,15 @@ import java.util.function.Supplier;
  */
 public class Integration {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void runOptional(String modid, Supplier<Runnable> runnable) {
         if (Platform.isModLoaded(modid)) {
-            runnable.get().run();
+            try {
+                runnable.get().run();
+            } catch (LinkageError | RuntimeException e) {
+                LOGGER.warn("Optional integration '{}' failed to initialize; continuing without it.", modid, e);
+            }
         }
     }
 
--- common/src/main/java/net/creeperhost/minetogethercommunity/compat/companion/FTBPackCompanionCompat.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/compat/companion/FTBPackCompanionCompat.java
@@ -1,15 +1,62 @@
 package net.creeperhost.minetogethercommunity.compat.companion;
 
-import dev.ftb.packcompanion.api.client.PackCompanionClientAPI;
-import dev.ftb.packcompanion.api.client.pause.AdditionalPauseTarget;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by brandon3055 on 14/07/2024
  */
 public class FTBPackCompanionCompat {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void init() {
-        PackCompanionClientAPI.get().registerAdditionalPauseProvider(AdditionalPauseTarget.TOP_RIGHT, new PauseProvider());
+        try {
+            PauseApi api = verifyPauseApi();
+            Object apiInstance = api.getMethod.invoke(null);
+            Object topRight = enumConstant(api.targetClass, "TOP_RIGHT");
+            api.registerMethod.invoke(apiInstance, topRight, new PauseProvider());
+        } catch (ReflectiveOperationException | LinkageError | RuntimeException e) {
+            LOGGER.warn("FTB Pack Companion pause API is not compatible; skipping MineTogether pause menu buttons.", e);
+        }
     }
 
+    private static PauseApi verifyPauseApi() throws ReflectiveOperationException {
+        Class<?> apiClass = Class.forName("dev.ftb.packcompanion.api.client.PackCompanionClientAPI");
+        Class<?> targetClass = Class.forName("dev.ftb.packcompanion.api.client.pause.AdditionalPauseTarget");
+        Class<?> providerClass = Class.forName("dev.ftb.packcompanion.api.client.pause.AdditionalPauseProvider");
+        Class<?> screenHolderClass = Class.forName("dev.ftb.packcompanion.api.client.pause.ScreenHolder");
+        Class<?> widgetCollectionClass = Class.forName("dev.ftb.packcompanion.api.client.pause.ScreenWidgetCollection");
+        Class<?> guiEventListenerClass = Class.forName("net.minecraft.client.gui.components.events.GuiEventListener");
+
+        Method getMethod = apiClass.getMethod("get");
+        Method registerMethod = apiClass.getMethod("registerAdditionalPauseProvider", targetClass, providerClass);
+
+        enumConstant(targetClass, "TOP_RIGHT");
+        providerClass.getMethod("init", targetClass, screenHolderClass, int.class, int.class);
+        screenHolderClass.getMethod("unsafeScreenAccess");
+        widgetCollectionClass.getMethod("create");
+        widgetCollectionClass.getMethod("addRenderableWidget", guiEventListenerClass);
+
+        return new PauseApi(targetClass, getMethod, registerMethod);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private static Object enumConstant(Class<?> enumClass, String name) {
+        return Enum.valueOf((Class) enumClass.asSubclass(Enum.class), name);
+    }
+
+    private static class PauseApi {
+        final Class<?> targetClass;
+        final Method getMethod;
+        final Method registerMethod;
+
+        PauseApi(Class<?> targetClass, Method getMethod, Method registerMethod) {
+            this.targetClass = targetClass;
+            this.getMethod = getMethod;
+            this.registerMethod = registerMethod;
+        }
+    }
 }
