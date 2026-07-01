0423ab8f753830d4b7a0bf3ab301345afa433304
Matthew <matt@creeperhost.net>
2026-06-30 21:20:17 -0700

    fix: remove UTF-8 BOM from ModPackInfo.java

--- common/src/main/java/net/creeperhost/minetogethercommunity/util/ModPackInfo.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/util/ModPackInfo.java
@@ -1,4 +1,4 @@
-﻿package net.creeperhost.minetogethercommunity.util;
+package net.creeperhost.minetogethercommunity.util;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.gson.Gson;
