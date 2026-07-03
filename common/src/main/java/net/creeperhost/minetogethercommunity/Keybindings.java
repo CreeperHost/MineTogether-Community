54028b5d87d55d41135a0f7ed283be697f4063f7
Gigabit101 <thegigabit101@gmail.com>
2026-07-03 17:01:17 +0100

    feat: add keybinding for opening cosmetics GUI
    Introduces a new keybinding (defaulting to 'C') to allow players to quickly access the cosmetics interface.

--- common/src/main/java/net/creeperhost/minetogethercommunity/Keybindings.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/Keybindings.java
@@ -6,12 +6,14 @@ import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import net.creeperhost.minetogethercommunity.chat.gui.FriendChatGui;
 import net.creeperhost.minetogethercommunity.chat.gui.PublicChatGui;
 import net.creeperhost.minetogethercommunity.cosmetic.CosmeticDownloader;
+import net.creeperhost.minetogethercommunity.cosmetic.CosmeticsGui;
 import net.creeperhost.minetogethercommunity.cosmetic.emote.EmoteFavorites;
 import net.creeperhost.minetogethercommunity.cosmetic.emote.EmotePlayer;
 import net.creeperhost.minetogethercommunity.cosmetic.emote.EmoteRadialScreen;
 import net.creeperhost.minetogethercommunity.gui.SettingGui;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.List;
 
@@ -25,6 +27,7 @@ public class Keybindings {
     public static final KeyMapping OPEN_FRIEND_CHAT = new KeyMapping("key.minetogether.friend_chat", InputConstants.Type.KEYSYM, InputConstants.UNKNOWN.getValue(), CATEGORY);
     public static final KeyMapping OPEN_GLOBAL_CHAT = new KeyMapping("key.minetogether.global_chat", InputConstants.Type.KEYSYM, InputConstants.UNKNOWN.getValue(), CATEGORY);
     public static final KeyMapping OPEN_SETTINGS = new KeyMapping("key.minetogether.settings", InputConstants.Type.KEYSYM, InputConstants.UNKNOWN.getValue(), CATEGORY);
+    public static final KeyMapping OPEN_COSMETICS = new KeyMapping("key.minetogether.cosmetics", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_C, CATEGORY);
     public static final KeyMapping OPEN_EMOTES = new KeyMapping("key.minetogether.emotes", InputConstants.Type.KEYSYM, InputConstants.UNKNOWN.getValue(), CATEGORY);
     public static final KeyMapping[] PLAY_FAVORITE_EMOTES = createFavoriteEmoteMappings();
 
@@ -32,6 +35,7 @@ public class Keybindings {
         KeyMappingRegistry.register(OPEN_FRIEND_CHAT);
         KeyMappingRegistry.register(OPEN_GLOBAL_CHAT);
         KeyMappingRegistry.register(OPEN_SETTINGS);
+        KeyMappingRegistry.register(OPEN_COSMETICS);
         KeyMappingRegistry.register(OPEN_EMOTES);
         for (KeyMapping mapping : PLAY_FAVORITE_EMOTES) {
             KeyMappingRegistry.register(mapping);
@@ -46,6 +50,8 @@ public class Keybindings {
             mc.setScreen(new PublicChatGui.Screen(null));
         } else if (OPEN_SETTINGS.consumeClick()) {
             mc.setScreen(new SettingGui.Screen(null));
+        } else if (OPEN_COSMETICS.consumeClick()) {
+            mc.setScreen(new CosmeticsGui.Screen(null));
         } else if (OPEN_EMOTES.consumeClick()) {
             mc.setScreen(new EmoteRadialScreen());
         }
