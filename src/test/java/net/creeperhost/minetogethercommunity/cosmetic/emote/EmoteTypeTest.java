74913d70e07edc8b89cb8cfe92006d341f4584a2
Aaron Mills <aaron_m@creeperhost.net>
2026-07-23 18:27:47 +0100

    refactor: handle unknown emote types and runtime availability checks

---
+++ src/test/java/net/creeperhost/minetogethercommunity/cosmetic/emote/EmoteTypeTest.java
@@ -0,0 +1,29 @@
+package net.creeperhost.minetogethercommunity.cosmetic.emote;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EmoteTypeTest {
+    @Test
+    public void missingTypeRemainsBackwardCompatible() {
+        assertEquals(EmoteType.SIMPLE, EmoteType.fromMetadata(null));
+        assertEquals(EmoteType.SIMPLE, EmoteType.fromMetadata("  "));
+    }
+
+    @Test
+    public void knownTypesAreCaseInsensitive() {
+        assertEquals(EmoteType.SIMPLE, EmoteType.fromMetadata(" SIMPLE "));
+        assertEquals(EmoteType.GECKOLIB, EmoteType.fromMetadata("GeckoLib"));
+    }
+
+    @Test
+    public void unknownTypesFailClosed() {
+        assertEquals(EmoteType.UNKNOWN, EmoteType.fromMetadata("future-runtime"));
+        assertFalse(EmoteType.UNKNOWN.isKnown());
+        assertFalse(EmoteType.UNKNOWN.isAvailable());
+        assertTrue(EmoteType.SIMPLE.isKnown());
+    }
+}
