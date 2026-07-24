22147c55ad12ce8b3c3ff7118269b2b615b42960
Aaron Mills <aaron_m@creeperhost.net>
2026-07-24 16:48:55 +0100

    fix: hide chat UI in commands-only mode

--- common/src/main/java/net/creeperhost/minetogethercommunity/chat/MineTogetherChat.java
+++ common/src/main/java/net/creeperhost/minetogethercommunity/chat/MineTogetherChat.java
@@ -31,6 +31,7 @@ import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.ChatVisiblity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
@@ -243,7 +244,8 @@ public class MineTogetherChat {
 
     private static boolean isMinecraftChatAllowed(Minecraft mc) {
         Minecraft.ChatStatus status = mc.getChatStatus();
-        return status != Minecraft.ChatStatus.DISABLED_BY_PROFILE
+        return mc.options.chatVisibility().get() == ChatVisiblity.FULL
+                && status != Minecraft.ChatStatus.DISABLED_BY_PROFILE
                 && status != Minecraft.ChatStatus.DISABLED_BY_LAUNCHER
                 && status != Minecraft.ChatStatus.DISABLED_BY_OPTIONS;
     }
