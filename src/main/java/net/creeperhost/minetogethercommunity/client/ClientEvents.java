9aa8eb22d6703e9180e778d3a2008f545a6747b5
Aaron Mills <aaron_m@creeperhost.net>
2026-07-31 17:46:30 +0100

    Confirm before opening links from chat

--- src/main/java/net/creeperhost/minetogethercommunity/client/ClientEvents.java
+++ src/main/java/net/creeperhost/minetogethercommunity/client/ClientEvents.java
@@ -27,6 +27,7 @@ import net.creeperhost.minetogethercommunity.gui.chat.PublicChatGui;
 import net.creeperhost.minetogethercommunity.oauth.KeycloakOAuth;
 import net.creeperhost.minetogethercommunity.oauth.ServerAuthTest;
 import net.creeperhost.minetogethercommunity.proxy.ClientProxy;
+import net.creeperhost.minetogethercommunity.util.ChatLinkOpener;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.client.gui.ChatLine;
@@ -216,9 +217,7 @@ public class ClientEvents {
 
         event.setCanceled(true);
         if (button == 0 && clickedUrl != null && !GuiScreen.isShiftKeyDown()) {
-            if (!KeycloakOAuth.openURL(clickedUrl)) {
-                MineTogetherChat.localStatus("minetogether.gui.chat.action.open_failed");
-            }
+            ChatLinkOpener.open(clickedUrl);
             return;
         }
         if (isActionableMessage(message) && button == 0 && !GuiScreen.isShiftKeyDown()) {
@@ -906,9 +905,7 @@ public class ClientEvents {
                 options.add(new PopupOption(I18n.format("minetogether.gui.chat.action.open_link"), 0x55AAFF, new Runnable() {
                     @Override
                     public void run() {
-                        if (!KeycloakOAuth.openURL(ChatActionPopup.this.url)) {
-                            MineTogetherChat.localStatus("minetogether.gui.chat.action.open_failed");
-                        }
+                        ChatLinkOpener.open(ChatActionPopup.this.url);
                     }
                 }));
             }
